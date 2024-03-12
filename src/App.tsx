4a6542a769861ddbacc86cf74cc4f143531ddba3
Nam Nguyen <nam.nguyen@shopify.com>
2024-03-12 15:03:54 -0300

    Add mounted logic 💞

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,30 @@ function App() {
   const [state, setState] = useState<"idle" | "loading" | "success" | "error">(
     "idle"
   );
+  const mountedRef = useRef<boolean>(false);
 
   const timeoutIdRef = useRef<number>();
   const debounceRef = useRef<DebounceFunction>();
   const abortControllerRef = useRef<AbortController>(new AbortController());
 
   useEffect(() => {
+    mountedRef.current = true;
+
     return () => {
       // This is optional, it's up to specific use case to decide if it's necessary to abort the ongoing request
       // when the component is unmounted.
       abortControllerRef.current.abort();
+
       clearTimeout(timeoutIdRef.current);
+      mountedRef.current = false;
     };
   }, []);
 
   const fetchNumber = async () => {
+    if (!mountedRef.current) {
+      return;
+    }
+
     setState("loading");
     const response = await fetch("/api/number", {
       signal: abortControllerRef.current.signal,
